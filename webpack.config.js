d6dd33f72aedc0b9078b70ed0729cab3579e64b4
cybuhh <cybuhh@users.noreply.github.com>
2020-03-15 13:09:10 +0100

    build tweaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.jsx',
   devtool: 'cheap-module-source-map',
+  optimization: {
+    splitChunks: {
+        chunks: 'all',
+        name: 'vendors',
+    },
+    runtimeChunk: true,
+  },
   resolve: {
     modules: ['node_modules', 'src'],
     extensions: ['.js', '.jsx'],
   },
+  externals: {
+    'chart.js': 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    filename: 'js/bundle.js',
+    publicPath: path.resolve(__dirname, 'public'),
     pathinfo: true,
   },
   devServer: {
@@ -55,13 +67,14 @@ module.exports = {
         },
         {
           loader: 'file-loader',
-          exclude: [/\.(js|jsx)$/, /\.html$/, /\.json$/],
+          exclude: [/\.jsx?$/, /\.html$/, /\.json$/],
         }
       ],
     
     }]
   },
   plugins: [
+    new webpack.HashedModuleIdsPlugin(),
     new HtmlWebpackPlugin({
       template: 'public/index.html',
     }),

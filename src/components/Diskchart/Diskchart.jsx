a679ec129bf668f819daadcd21dd8a1a5852caf0
cybuhh <cybuhh@users.noreply.github.com>
2020-03-16 22:58:12 +0100

    add memo for chart component

--- src/components/Diskchart/Diskchart.jsx
+++ src/components/Diskchart/Diskchart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, memo } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import Chart from 'chart.js';
@@ -18,17 +18,17 @@ function Diskchart({ title, className }) {
             data: [70, 30],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
@@ -62,4 +62,4 @@ Diskchart.defaultProps = {
     className: ""
 };
 
-export default Diskchart;
+export default memo(Diskchart);
